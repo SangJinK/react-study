df335ce98040ab7af623677d1728179b62b6c727
sangjin <enrwls123@gmail.com>
2024-05-07 15:10:41 +0900

    useReducer 로 이메일 상태 처리

--- src/components/SideEffect/Login/Login.jsx
+++ src/components/SideEffect/Login/Login.jsx
@@ -1,14 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import styles from './Login.module.css';
 import Card from '../../UI/Card';
 import Button from '../../UI/Button/Button';
+/*
+  이 컴포넌트에서 사용하는 모든 상태와 상태 변경을 중앙 제어하는 함수.
+  컴포넌트 내부 데이터를 사용하지 않고 상태에만 집중하기 때문에
+  컴포넌트 바깥쪽에 선언하는 것이 일반적입니다.
+  param1 - state: 변경 전의 상태값
+  param2 - action: dispatch함수(상태 변경 등의 행동)가 전달한 상태 변경 객체
+  return: 관리할 상태값들을 반환
+*/
+
+const emailReducer = (state, action) => {
+  console.log('emailReducer called!');
+  console.log('state: ', state);
+  console.log('action: ', action);
+
+  // 상태 변경을 위한 객체
+  if (action.type === 'USER_INPUT') {
+    return {
+      value: action.value,
+      isValid: action.value.includes('@'),
+    };
+  } else if (action.type === 'INPUT_VALIDATE') {
+    return {
+      value: state.value,
+      isValid: state.value.includes('@'),
+    };
+  }
+};
 
 const Login = ({ onLogin }) => {
-  // 이메일 입력값을 저장
-  const [enteredEmail, setEnteredEmail] = useState('');
+  /*
+    param1 - reducer function: 위에서 만든 리듀서 함수
+    param2 - initial state: 초기 상태값
+    return1 - 이메일 관련 상태변수
+    return2 - dispatch함수: 상태를 변경할 수 있는 함수
+  */
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
 
-  // 이메일 입력이 정상적인지 확인
-  const [emailIsValid, setEmailIsValid] = useState();
+  // 이메일 입력값을 저장
+
   // 패스워드 입력값을 저장
   const [enteredPassword, setEnteredPassword] = useState('');
   // 패스워드 입력이 정상적인지 확인
@@ -16,22 +51,25 @@ const Login = ({ onLogin }) => {
   // 이메일, 패스워드가 둘 다 동시에 정상적인 상태인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const { isValid: emailIsValid } = emailState;
+
   useEffect(() => {
     const timer = setTimeout(() => {
       console.log('gd');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6,
-      );
+      setFormIsValid(emailIsValid && enteredPassword.trim().length > 6);
     }, 1000);
 
     return () => {
       console.log('clean up!');
       clearTimeout(timer);
     };
-  }, [enteredEmail, enteredPassword]);
+  }, [emailIsValid, enteredPassword]);
 
   const emailChangeHandler = (e) => {
-    setEnteredEmail(e.target.value);
+    dispatchEmail({
+      type: 'USER_INPUT',
+      value: e.target.value,
+    });
   };
 
   const passwordChangeHandler = (e) => {
@@ -39,7 +77,9 @@ const Login = ({ onLogin }) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({
+      type: 'INPUT_VALIDATE',
+    });
   };
 
   const validatePasswordHandler = () => {
@@ -48,7 +88,7 @@ const Login = ({ onLogin }) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    onLogin(enteredEmail, enteredPassword);
+    onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -61,7 +101,7 @@ const Login = ({ onLogin }) => {
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
