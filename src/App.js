efc259f699fe1a8de0cc9b5b8d463a9ad39051ec
sangjin <enrwls123@gmail.com>
2024-05-07 11:58:34 +0900

    Side Effect 템플릿 및 문제사항 확인

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import React, { useState } from 'react';
-import AddUsers from './components/Users/AddUsers';
-import UserList from './components/Users/UserList';
+
+import MainHeader from './components/SideEffect/MainHeader/MainHeader';
+import Login from './components/SideEffect/Login/Login';
+import Home from './components/SideEffect/Home/Home';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  console.log('로그인 검사 수행');
+  const storedLoginFlag = localStorage.getItem('login-flag');
+
+  if (storedLoginFlag === '1') {
+    setIsLoggedIn(true);
+  }
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('login-flag', '1');
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div>
-      <AddUsers />
-      <UserList />
-    </div>
+    <>
+      <MainHeader />
+      <main>
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+      </main>
+    </>
   );
 };
 
