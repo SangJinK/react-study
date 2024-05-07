c2960bf3ba34533249206eb72e87eec80ce27246
sangjin <enrwls123@gmail.com>
2024-05-07 12:27:01 +0900

    useEffect 기초

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Login from './components/SideEffect/Login/Login';
@@ -8,21 +8,27 @@ import Home from './components/SideEffect/Home/Home';
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  console.log('로그인 검사 수행');
-  const storedLoginFlag = localStorage.getItem('login-flag');
+  useEffect(() => {
+    console.log('로그인 검사 수행');
+    const storedLoginFlag = localStorage.getItem('login-flag');
 
-  if (storedLoginFlag === '1') {
-    setIsLoggedIn(true);
-  }
+    if (storedLoginFlag === '1') {
+      setIsLoggedIn(true);
+    }
+  });
 
   const loginHandler = (email, password) => {
     localStorage.setItem('login-flag', '1');
     setIsLoggedIn(true);
   };
+  const logoutHandler = () => {
+    localStorage.setItem('login-flag', '0');
+    setIsLoggedIn(false);
+  };
 
   return (
     <>
-      <MainHeader />
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
