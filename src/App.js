21e33dc7c996b884daf7933af166ef22fdbced32
sangjin <enrwls123@gmail.com>
2024-05-07 16:40:04 +0900

    useContext와 리팩토링

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React, { useEffect, useState } from 'react';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Login from './components/SideEffect/Login/Login';
 import Home from './components/SideEffect/Home/Home';
+import AuthContext from './components/store/auth-context';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    console.log('로그인 검사 수행');
     const storedLoginFlag = localStorage.getItem('login-flag');
 
     if (storedLoginFlag === '1') {
@@ -27,13 +27,18 @@ const App = () => {
   };
 
   return (
-    <>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <MainHeader />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
       </main>
-    </>
+    </AuthContext.Provider>
   );
 };
 
--- src/components/SideEffect/Navi/Navigation.jsx
+++ src/components/SideEffect/Navi/Navigation.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Navigation.module.css';
-const Navigation = (props) => {
+import AuthContext from '../../store/auth-context';
+const Navigation = () => {
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={onLogout}>Logout</button>
           </li>
         )}
       </ul>
